71610a48828be92fd07ef7e64235608ca0d6eb77
Rumberg Vitalii <modtitiof@gmail.com>
2023-09-13 11:48:11 +0300

    base mode

--- src/js/task-07.js
+++
@@ -1,5 +0,0 @@
-const progress = document.querySelector('#font-size-control');
-const text = document.querySelector('#text');
-progress.addEventListener("input", () => {
-    text.style.fontSize = progress.value.toString() + 'px';
-})
